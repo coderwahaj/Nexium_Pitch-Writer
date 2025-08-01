d8778d83c8ef86c8546ad33b7e9439723123fe43
coderwahaj <wahajasif488@gmail.com>
2025-08-01 16:28:29 +0500

    feat:final commit ,everything done.

--- pitch-writer/src/app/result/page.tsx
+++ pitch-writer/src/app/result/page.tsx
@@ -2,10 +2,6 @@
 
 import { Suspense } from "react";
 import ResultClient from "./ResultClient";
-
-
-
-
 export default function Page() {
   return (
     <Suspense fallback={<div className="text-center mt-10">Loading...</div>}>
